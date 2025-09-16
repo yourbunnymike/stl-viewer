db6b1f497fb0835960c0c73740744a118b76acd1
yourbunnymike <hike1mawk@gmail.com>
2025-09-17 01:03:22 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+
+    mainWindow.loadFile('stl-viewer.html');
+    
+    // Открыть DevTools в режиме разработки
+    if (!app.isPackaged) {
+        mainWindow.webContents.openDevTools();
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
